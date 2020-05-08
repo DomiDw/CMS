5488db8b286083cdcee4f367f81a373606e30dc1
Marvin Loeffler <marvin.loeffler@aderion.de>
2020-05-08 13:56:08 +0200

    fixed input width

--- src/components/Textbox/Textbox.tsx
+++ src/components/Textbox/Textbox.tsx
@@ -4,7 +4,7 @@ import './textbox.scss'
 
 export function Textbox(props: ITextboxProps) {
     return (
-        <div className="textbox-block">
+        <div className="textbox-block col-xs-12">
             <input className="textbox" placeholder="Hier können Sie Ihre Beschreibung eintragen">
             </input>
         </div>
