b654202ef40cd148ea95bd01b36983bc04a6c489
Dwinger91 <dominik.dwinger@soccerwatch.tv>
2020-05-08 16:03:30 +0200

    changed fontcolor / textblock element

--- src/components/Textbox/Textbox.tsx
+++ src/components/Textbox/Textbox.tsx
@@ -5,8 +5,9 @@ import './textbox.scss'
 export function Textbox(props: ITextboxProps) {
     return (
         <div className="textbox-block col-xs-12">
-            <input className="textbox" placeholder="Hier können Sie Ihre Beschreibung eintragen">
-            </input>
+            <p className="textbox" placeholder="Hier können Sie Ihre Beschreibung eintragen">
+                Hier könnte Ihre Werbung stehen
+            </p>
         </div>
     )
 }
