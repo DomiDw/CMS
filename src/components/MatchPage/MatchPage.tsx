fff1ee5509b298d3d2f890185f2635fb47e5e826
Dwinger91 <dominik.dwinger@soccerwatch.tv>
2020-07-06 10:22:13 +0200

    backup case before major css change

--- src/components/MatchPage/MatchPage.tsx
+++ src/components/MatchPage/MatchPage.tsx
@@ -7,7 +7,6 @@ import { IMatchPageState, IMatchPageProps } from './IMatchPage'
 import axios from 'axios'
 import Discovery from '@soccerwatch/discovery'
 import * as ReactBootsTrap from 'react-bootstrap'
-import { Navbar } from 'react-bootstrap'
 
 class MatchPage extends Component<IMatchPageProps, IMatchPageState> {
   backToPage:string = 'https://europe-west1-sw-sc-de-dev.cloudfunctions.net/aisw-cms-clubpage/' + this.getClubIdFromUrl()
@@ -39,110 +38,110 @@ class MatchPage extends Component<IMatchPageProps, IMatchPageState> {
     return metaDataVideo
   }
 
-getData = async () => {
-  const video = await this.getVideo()
-  const metaDataVideo = video
-  const clubAPIHome = Discovery.API_CLUB + '/info/' + metaDataVideo.clubAId
-  const clubAPIGuest = Discovery.API_CLUB + '/info/' + metaDataVideo.clubBId
-  const clubAPI = Discovery.API_CLUB + '/info/' + this.getClubIdFromUrl()
-  const res = await Promise.all([
-    axios.get(clubAPIHome),
-    axios.get(clubAPIGuest),
-    axios.get(clubAPI)
-  ])
-  const metaDataClubHome = res[0].data
-  const metaDataClubGuest = res[1].data
-  const metaDataClub = res[2].data
-  this.setState({ metaDataVideo, metaDataClubHome, metaDataClubGuest, metaDataClub, loading: false })
-}
+  getData = async () => {
+    const video = await this.getVideo()
+    const metaDataVideo = video
+    const clubAPIHome = Discovery.API_CLUB + '/info/' + metaDataVideo.clubAId
+    const clubAPIGuest = Discovery.API_CLUB + '/info/' + metaDataVideo.clubBId
+    const clubAPI = Discovery.API_CLUB + '/info/' + this.getClubIdFromUrl()
+    const res = await Promise.all([
+      axios.get(clubAPIHome),
+      axios.get(clubAPIGuest),
+      axios.get(clubAPI)
+    ])
+    const metaDataClubHome = res[0].data
+    const metaDataClubGuest = res[1].data
+    const metaDataClub = res[2].data
+    this.setState({ metaDataVideo, metaDataClubHome, metaDataClubGuest, metaDataClub, loading: false })
+  }
 
-componentDidMount () {
-  this.getData()
-}
+  componentDidMount () {
+    this.getData()
+  }
 
-render () {
-  return (
-    <div className='container mx-auto'>
-      <div className='row'>
-        <Navbar className='navBar'>
-          <button>
-            <a className='navButton' href={this.backToPage} target='blank'>
-              Zurück
-            </a>
-          </button>
-        </Navbar>
-      </div>
-      <div className='row'>
-        {this.state.loading
-          ? (
-            <div className='mx-auto'>
-              <ReactBootsTrap.Spinner animation='border' />
-            </div>)
-          : (
-            <div style={{ width: '100%' }}>
-              <Video
-                url={
-                  this.state.metaDataVideo
-                    ? this.state.metaDataVideo.userStream
-                    : ''
-                }
-              />
-              <div className='flex flex-wrap'>
-                <div className='w-full mb-4'>
-                  <Team
-                    home={{
-                      name: this.state.metaDataClubHome
-                        ? this.state.metaDataClubHome.name
-                        : '',
-                      thumbnail: this.state.metaDataClubHome
-                        ? this.state.metaDataClubHome.thumbnail
-                        : ''
-                    }}
-                    score={{
-                      home: this.state.metaDataVideo
-                        ? this.state.metaDataVideo.scoreA
-                        : '',
-                      guest: this.state.metaDataVideo
-                        ? this.state.metaDataVideo.scoreB
-                        : ''
-                    }}
-                    guest={{
-                      name: this.state.metaDataClubGuest
-                        ? this.state.metaDataClubGuest.name
-                        : '',
-                      thumbnail: this.state.metaDataClubGuest
-                        ? this.state.metaDataClubGuest.thumbnail
-                        : ''
-                    }}
-                  />
+  render () {
+    return (
+      <div className='container mx-auto'>
+        <div className='row'>
+          <ReactBootsTrap.Navbar className='navBar' fixed='top'>
+            <button>
+              <a className='navButton' href={this.backToPage} target='blank'>
+                Zurück
+              </a>
+            </button>
+          </ReactBootsTrap.Navbar>
+        </div>
+        <div className='row'>
+          {this.state.loading
+            ? (
+              <div className='mx-auto'>
+                <ReactBootsTrap.Spinner animation='border' />
+              </div>)
+            : (
+              <div style={{ width: '100%' }}>
+                <Video
+                  url={
+                    this.state.metaDataVideo
+                      ? this.state.metaDataVideo.userStream
+                      : ''
+                  }
+                />
+                <div className='flex flex-wrap'>
+                  <div className='w-full mb-4'>
+                    <Team
+                      home={{
+                        name: this.state.metaDataClubHome
+                          ? this.state.metaDataClubHome.name
+                          : '',
+                        thumbnail: this.state.metaDataClubHome
+                          ? this.state.metaDataClubHome.thumbnail
+                          : ''
+                      }}
+                      score={{
+                        home: this.state.metaDataVideo
+                          ? this.state.metaDataVideo.scoreA
+                          : '',
+                        guest: this.state.metaDataVideo
+                          ? this.state.metaDataVideo.scoreB
+                          : ''
+                      }}
+                      guest={{
+                        name: this.state.metaDataClubGuest
+                          ? this.state.metaDataClubGuest.name
+                          : '',
+                        thumbnail: this.state.metaDataClubGuest
+                          ? this.state.metaDataClubGuest.thumbnail
+                          : ''
+                      }}
+                    />
+                  </div>
                 </div>
+                <div className='clubDescriptionText'>
+                  Vereinsbeschreibung
+                </div>
+                <TextBox
+                  editableText={
+                    this.state.metaDataClub
+                      ? this.state.metaDataClub.location
+                      : ''
+                  }
+                />
+                <div className='clubMessageText'>
+                  Vereinsnachricht
+                </div>
+                <TextBox
+                  editableText={
+                    this.state.metaDataClub
+                      ? this.state.metaDataClub.location
+                      : ''
+                  }
+                />
               </div>
-              <div className='clubDescriptionText'>
-                Vereinsbeschreibung
-              </div>
-              <TextBox
-                editableText={
-                  this.state.metaDataClub
-                    ? this.state.metaDataClub.location
-                    : ''
-                }
-              />
-              <div className='clubMessageText'>
-                Vereinsnachricht
-              </div>
-              <TextBox
-                editableText={
-                  this.state.metaDataClub
-                    ? this.state.metaDataClub.location
-                    : ''
-                }
-              />
-            </div>
-          )}
+            )}
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 export default MatchPage
