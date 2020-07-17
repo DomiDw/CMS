7ce3c56f6e0617c0bc10d417a47cd554d03a028a
Dwinger91 <dominik.dwinger@soccerwatch.tv>
2020-07-17 11:28:26 +0200

    Fixed responsive css for tablematch

--- src/components/TableMatch/TableMatch.tsx
+++ src/components/TableMatch/TableMatch.tsx
@@ -8,6 +8,7 @@ import axiosRetry from 'axios-retry'
 import { Spinner } from '../Spinner/Spinner'
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp'
+// import { Link } from 'react-router-dom'
 
 export class TableMatch extends Component<ITableMatchProps, any> {
   constructor (props: ITableMatchProps) {
@@ -18,8 +19,8 @@ export class TableMatch extends Component<ITableMatchProps, any> {
       showFuture: false,
       sortDirection: 'asc',
       configTableHeader: [
-        { name: 'clubATeam', showName: 'Ihr Verein' },
-        { name: 'clubBTeam', showName: 'Gegn. Verein' },
+        { name: 'clubATeam', showName: 'Heim Mannschaft' },
+        { name: 'clubBTeam', showName: 'Gast Mannschaft' },
         { name: 'gameDay', showName: 'Datum' }
       ],
       rows: []
@@ -30,12 +31,15 @@ export class TableMatch extends Component<ITableMatchProps, any> {
     axiosRetry(axios, { retries: 5 })
     const pastFirstMatchAPI = Discovery.API_VIDEO + '/meta/41651'
     const pastSecondMatchAPI = Discovery.API_VIDEO + '/meta/41196'
+    const pastthirdMatchAPI = Discovery.API_VIDEO + '/meta/37400'
     const res = await Promise.all([
       axios.get(pastFirstMatchAPI),
-      axios.get(pastSecondMatchAPI)
+      axios.get(pastSecondMatchAPI),
+      axios.get(pastthirdMatchAPI)
     ])
     const pastFirstMatchData = res[0].data
     const pastSecondMatchData = res[1].data
+    const pastThirdMatchData = res[2].data
     this.setState({
       rows: [
         {
@@ -47,6 +51,11 @@ export class TableMatch extends Component<ITableMatchProps, any> {
           clubATeam: pastSecondMatchData.clubATeam,
           clubBTeam: pastSecondMatchData.clubBTeam,
           gameDay: pastSecondMatchData.gameDay
+        },
+        {
+          clubATeam: pastThirdMatchData.clubATeam,
+          clubBTeam: pastThirdMatchData.clubBTeam,
+          gameDay: pastThirdMatchData.gameDay
         }
       ],
       loading: false
@@ -60,48 +69,44 @@ export class TableMatch extends Component<ITableMatchProps, any> {
   showHideTablePast = () => {
     return (
       this.state.showPast === true ? (
-        <div className='col-xs-6'>
-          <div className='row'>
-            <table className='past-games'>
-              <caption>PAST</caption>
-              <thead>
-                <tr>
-                  {this.state.configTableHeader.map((header:any, i:number) => (
-                    <th
-                      key={i}
-                      className='sortHeader'
-                      onClick={() => this.sortArray(header.name)}
-                    >
-                      <span>
-                        {header.showName}
-                        <span className='sortButton'>
-                          {this.state.sortDirection === 'asc'
-                            ? <ArrowDropDownIcon />
-                            : <ArrowDropUpIcon />}
-                        </span>
-                      </span>
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.rows.map((row: any, i:number) => (
-                  <tr key={i}>
-                    <td>
-                      {row.clubATeam}
-                    </td>
-                    <td>
-                      {row.clubBTeam}
-                    </td>
-                    <td>
-                      {row.gameDay}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
+        <table className='past-games'>
+          <caption>PAST</caption>
+          <thead>
+            <tr>
+              {this.state.configTableHeader.map((header:any, i:number) => (
+                <th
+                  key={i}
+                  className='sortHeader'
+                  onClick={() => this.sortArray(header.name)}
+                >
+                  <span>
+                    {header.showName}
+                    <span className='sortButton'>
+                      {this.state.sortDirection === 'asc'
+                        ? <ArrowDropDownIcon />
+                        : <ArrowDropUpIcon />}
+                    </span>
+                  </span>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.rows.map((row: any, i:number) => (
+              <tr key={i}>
+                <td>
+                  {row.clubATeam}
+                </td>
+                <td>
+                  {row.clubBTeam}
+                </td>
+                <td>
+                  {row.gameDay}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       ) : null
     )
   }
@@ -109,48 +114,44 @@ export class TableMatch extends Component<ITableMatchProps, any> {
   showHideTableFuture = () => {
     return (
       this.state.showFuture === true ? (
-        <div className='col-xs-6'>
-          <div className='row'>
-            <table className='upcoming-games'>
-              <caption>FUTURE</caption>
-              <thead>
-                <tr>
-                  {this.state.configTableHeader.map((header:any, i:number) => (
-                    <th
-                      key={i}
-                      className='sortHeader'
-                      onClick={() => this.sortArray(header.name)}
-                    >
-                      <span>
-                        {header.showName}
-                        <span className='sortButton'>
-                          {this.state.sortDirection === 'asc'
-                            ? <ArrowDropDownIcon />
-                            : <ArrowDropUpIcon />}
-                        </span>
-                      </span>
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.rows.map((row: any, i:number) => (
-                  <tr key={i}>
-                    <td>
-                      {row.clubATeam}
-                    </td>
-                    <td>
-                      {row.clubBTeam}
-                    </td>
-                    <td>
-                      {row.gameDay}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
+        <table className='upcoming-games'>
+          <caption>FUTURE</caption>
+          <thead>
+            <tr>
+              {this.state.configTableHeader.map((header:any, i:number) => (
+                <th
+                  key={i}
+                  className='sortHeader'
+                  onClick={() => this.sortArray(header.name)}
+                >
+                  <span>
+                    {header.showName}
+                    <span className='sortButton'>
+                      {this.state.sortDirection === 'asc'
+                        ? <ArrowDropDownIcon />
+                        : <ArrowDropUpIcon />}
+                    </span>
+                  </span>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.rows.map((row: any, i:number) => (
+              <tr key={i}>
+                <td>
+                  {row.clubATeam}
+                </td>
+                <td>
+                  {row.clubBTeam}
+                </td>
+                <td>
+                  {row.gameDay}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       ) : null
     )
   }
@@ -190,34 +191,50 @@ export class TableMatch extends Component<ITableMatchProps, any> {
       <>
         <div className='col-xs-12'>
           <div className='row'>
-            <div className='headerButtons'>
-              <button
-                className='buttonz'
-                onClick={() => {
-                  this.setState({
-                    showPast: !this.state.showPast
-                  })
-                }}
-              >
-                Spielplan vergangener Spiele
-              </button>
-              <button
-                className='buttonz'
-                onClick={() => {
-                  this.setState({
-                    showFuture: !this.state.showFuture
-                  })
-                }}
-              >
-                Spielplan künftiger Spiele
-              </button>
+            <div className='col-xs-12 col-sm-12 col-md-6 col-center'>
+              <div className='row'>
+                <div>
+                  <div className='col-xs-12 left-Side'>
+                    <button
+                      className='buttonz'
+                      onClick={() => {
+                        this.setState({
+                          showPast: !this.state.showPast
+                        })
+                      }}
+                    >
+                      Spielplan vergangener Spiele
+                    </button>
+                  </div>
+                  <div className='col-xs-12'>
+                    {this.showHideTablePast()}
+                  </div>
+                  <div className='spacer-small' />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className='col-xs-12'>
-          <div className='row'>
-            {this.showHideTablePast()}
-            {this.showHideTableFuture()}
+            <div className='col-xs-12 col-sm-12 col-md-6 col-center'>
+              <div className='row'>
+                <div>
+                  <div className='col-xs-12 right-Side'>
+                    <button
+                      className='buttonz'
+                      onClick={() => {
+                        this.setState({
+                          showFuture: !this.state.showFuture
+                        })
+                      }}
+                    >
+                      Spielplan künftiger Spiele
+                    </button>
+                  </div>
+                  <div className='col-xs-12'>
+                    {this.showHideTableFuture()}
+                  </div>
+                  <div className='spacer-small' />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </>
